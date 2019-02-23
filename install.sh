e51b1c8be1af8ac5ce7f29a81c7c171c44575f3f
guri <guri@gurinoMacBook-Air.local>
2019-02-24 00:21:53 +0900

    chg shebang & ignore bash_history

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# 未定義変数があれば表示する
+set -u
+
+# エラー発生時、処理を中断する
+# set -e
 
 echo "symbolic link dotfiles..."
 for f in .??*; do
     [ "$f" = ".git" ] && continue;
     [ "$f" = ".gitignore" ] && continue;
-    [ "$f" = ".DS_Store" ] && continue; # macの場合、このカスみたいなファイルも無視する。
+    [ "$f" = ".DS_Store" ] && continue; # macの場合、このカスみたいなファイルも無視する
 
-    # HOMEに各dotfileのシンボリックリンクを貼る。
+    # HOMEに各dotfileのシンボリックリンクを貼る
     ln -snfv ~/dotfiles/"$f" ~/
 done
 
