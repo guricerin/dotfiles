478164a00293b8735bd9779f525ea575580ad490
guri <guri@gurinoMacBook-Air.local>
2019-02-26 21:21:49 +0900

    stealed dotfiles logo

--- install.sh
+++ install.sh
@@ -2,10 +2,77 @@
 
 # 未定義変数があれば表示する
 set -u
-
 # エラー発生時、処理を中断する
-# set -e
+set -e
 
+# use colors on terminal
+tput=$(which tput)
+if [ -n "$tput" ]; then
+    ncolors=$($tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+fi
+
+### functions
+# info: output terminal green
+info() {
+    printf "${GREEN}"
+    echo -n "  info  "
+    printf "${NORMAL}"
+    echo "$1"
+}
+# error: output terminal red
+error() {
+    printf "${RED}"
+    echo -n "  error "
+    printf "${NORMAL}"
+    echo "$1"
+}
+# warn: output terminal yellow
+warn() {
+    printf "${YELLOW}"
+    echo -n "  warn  "
+    printf "${NORMAL}"
+    echo "$1"
+}
+# log: out put termial normal
+log() {
+    echo "  $1"
+}
+
+dotfiles_logo='
+██████╗  ██████╗ ████████╗███████╗██╗██╗     ███████╗███████╗
+██╔══██╗██╔═══██╗╚══██╔══╝██╔════╝██║██║     ██╔════╝██╔════╝
+██║  ██║██║   ██║   ██║   █████╗  ██║██║     █████╗  ███████╗
+██║  ██║██║   ██║   ██║   ██╔══╝  ██║██║     ██╔══╝  ╚════██║
+██████╔╝╚██████╔╝   ██║   ██║     ██║███████╗███████╗███████║
+╚═════╝  ╚═════╝    ╚═╝   ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝
+'
+echo "$dotfiles_logo"
+
+read -p "$(warn 'Are you sure you want to install it? [y/n] ')" -n 1 -r
+
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo ""
+    error 'Installation failed. Nothing changed.'
+    exit 1
+fi
+
+
+exit 0
 echo "symlink dotfiles..."
 for f in .??*; do
     [ "$f" = ".git" ] && continue;
