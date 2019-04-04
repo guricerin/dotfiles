d1fdd0ef58e6f60fa50267279a52647e014b3849
guri <gurizumi@gmail.com>
2019-04-04 03:08:41 +0000

    add laxterminal

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,4 +1,5 @@
 function fish_greeting
-    echo Welcome to fish, the friendly interactive shell!
+    #echo Welcome to fish, the friendly interactive shell!
+		echo "なんやねんこの深海魚は"
     fish_logo
 end
