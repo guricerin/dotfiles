a07773887e0a266eb245bcdc916b33aaabedecce
guricerin <gurizumi@gmail.com>
2019-04-25 03:25:25 +0000

    add alias tree to fish

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -7,5 +7,9 @@ alias cp 'cp -i'
 # cd したら ll する
 functions --copy cd standard_cd
 function cd
- standard_cd $argv; and ll
+    standard_cd $argv; and ll
+end
+
+function tree
+    pwd;find . | sort | sed '1d;s/^\.//;s/\/\([^/]*\)$/|--\1/;s/\/[^/|]*/| /g'
 end
