943d98d3af978e9ab555ea0262899a8baf92c368
guricerin <chanbo1e9@gmail.com>
2025-02-28 15:37:52 +0900

    色々修正

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -u
 # エラー発生時、処理を中断する
 set -e
 
-dotfiles_logo='
+readonly LOGO='
     _        _    __ _ _
    | |      | |  / _(_) |
  __| |  ___ | |_| |_ _| | ___  ___
@@ -14,25 +14,31 @@ dotfiles_logo='
  \____|\___/ \__|_| |_|_|\___||___/
 '
 
-readonly SCRIPT_ROOT=$(cd $(dirname ${0}); pwd)
-readonly DOTFILES_HOME=${SCRIPT_ROOT}/home
+readonly SCRIPT_ROOT=$(cd $(dirname $0); pwd)
+readonly DOTFILES_HOME="$SCRIPT_ROOT/home"
 
-main() {
-    # ディレクトリのシムリンクは作成しない
-    for src in $(find ${DOTFILES_HOME}/ -type f); do
-        local dst=${src/${DOTFILES_HOME}/$(echo $HOME)} # 文字列置換
-        local parent=$(dirname ${dst})
-        if [ ! -d ${parent} ]; then
-            mkdir -p ${parent}
-        fi
-        if [ -f dst ]; then
-            cp dst ${dst}.bak
-        fi
-        ln -snf ${src} ${dst}
-        printf "\e[0;32m  [o] ${dst} -> ${src}\e[0m\n"
-    done
+_build_symlink() {
+  # 意図しないファイルがリポジトリに含まれるのを防止したいので、ディレクトリのシムリンクは作成しない
+  for src in $(find $DOTFILES_HOME -type f); do
+    echo "---"
+    local dst=${src/$DOTFILES_HOME/$(echo $HOME)} # 文字列置換
+    local parent=$(dirname $dst)
+    if [ ! -d $parent ]; then
+      mkdir -p $parent
+    fi
+    if [ -f $dst ]; then
+      printf "\e[0;34m      backup $dst.bak\e[0m\n"
+      cp $dst $dst.bak
+    fi
+    ln -snf $src $dst
+    printf "\e[0;32m  [o] $dst -> $src\e[0m\n"
+  done
 }
 
-echo "$dotfiles_logo"
-main
-echo ""
+_main() {
+  echo "$LOGO"
+  _build_symlink
+  echo -e "\n ようこそ、dotfiles へ。\n そして、あなたの帰還に感謝します。"
+}
+
+_main
