214469f338766182f8d099c6657519171c0136c4
guricerin <chanbo1e9@gmail.com>
2025-03-01 21:14:38 +0900

    chg repo name

--- setup.sh
+++ setup.sh
@@ -5,23 +5,23 @@ set -u
 # エラー発生時、処理を中断する
 set -e
 
-# https://patorjk.com/software/taag/#p=display&f=Efti%20Wall&t=dotfiles
+# https://patorjk.com/software/taag/#p=display&f=Efti%20Wall&t=homebound
 readonly LOGO=$'
-        ...       #   ___                          _          .      .      _     _         |"|
-   o,*,(o o)      #  <_*_>         ()_()         _|_|_      .  .:::.      o\' \,=./ `o      _|_|_       `  _ ,  \'
-  8(o o)(_)Ooo    #  (o o)         (o o)         (o o)        :(o o):  .     (o o)         (o o)      -  (o)o)  -
-ooO-(_)---Ooo-----8---(_)--Ooo-ooO--`o\'--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo--ooO\'(_)--Ooo-
+     ___       #   ___            ___          |"|          ( ( (      #   ___            o           _   _             ...
+    .|||.      #  <_*_>          /_\ `*       _|_|_       \'. ___ .\'    #  <_*_>        ` /_\ \'       \'\\-//`       o,*,(o o)
+    (o o)      #  (o o)         (o o)         (o o)      \'  (> <) \'    #  (o o)       - (o o) -       (o o)       8(o o)(_)Ooo
+ooO--(_)--Ooo--8---(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo--8---(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO-(_)---Ooo----
 '
 
 readonly SCRIPT_ROOT=$(cd $(dirname $0); pwd)
-readonly DOTFILES_HOME_ABS_PATH="$SCRIPT_ROOT/home"
+readonly HOMEBOUND_ABS_PATH="$SCRIPT_ROOT/home"
 
 build_symlink() {
-  echo "Start to build symlink."
+  echo "Start to build symlinks."
   # 意図しないファイルがリポジトリに含まれるのを防止したいので、ディレクトリのシムリンクは作成しない
-  for src in $(find $DOTFILES_HOME_ABS_PATH -type f); do
+  for src in $(find $HOMEBOUND_ABS_PATH -type f); do
     echo "---"
-    local dst=${src/$DOTFILES_HOME_ABS_PATH/$(echo $HOME)} # e.g. /home/username/dotfiles/home/.zshrc => /home/username/.zshrc
+    local dst=${src/$HOMEBOUND_ABS_PATH/$(echo $HOME)} # e.g. /home/username/homebound/home/.zshrc => /home/username/.zshrc
     local parent=$(dirname $dst)
     if [ ! -d $parent ]; then
       mkdir -p $parent
